addb8710ad085f44dc29b81ede21193a828ed600
ngoquanganhalb <ngoquanganhalb@gmail.com>
2025-07-01 12:51:47 +0700

    30-5-2025

--- js/test.ts
+++ js/test.ts
@@ -1,24 +1,29 @@
-// Input: s = "anagram", t = "nagaram"
+// // Input: s = "anagram", t = "nagaram"
 
-// Output: true
+// // Output: true
 
-function isAnagram(s: string, t: string): boolean {
-  const arr1 = s.split('');
-  const arr2 = t.split('')
-  const obj1 : Record<string,number> = {} ; 
-  const obj2 :Record<string,number> = {}
-  for(const key of arr1){
-    obj1[key] = (obj1[key]||0)+1
-  }
-  arr2.forEach(key=> obj2[key]=(obj2[key]||0)+1)
-  if(Object.values(obj1).length!==Object.values(obj2).length) return false
-  for(const key in obj1) {
-    console.log(obj1.key)
-    if(obj1[key]!== obj2[key]) 
-      return false
-  }
-  return true
-};
+// function isAnagram(s: string, t: string): boolean {
+//   const arr1 = s.split('');
+//   const arr2 = t.split('')
+//   const obj1 : Record<string,number> = {} ;
+//   const obj2 :Record<string,number> = {}
+//   for(const key of arr1){
+//     obj1[key] = (obj1[key]||0)+1
+//   }
+//   arr2.forEach(key=> obj2[key]=(obj2[key]||0)+1)
+//   if(Object.values(obj1).length!==Object.values(obj2).length) return false
+//   for(const key in obj1) {
+//     console.log(obj1.key)
+//     if(obj1[key]!== obj2[key])
+//       return false
+//   }
+//   return true
+// };
 
-console.log(isAnagram('rat','carr'))
+// console.log(isAnagram('rat','carr'))
 
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+function longestCommonPrefix(strs: string[]) {
+  
+}
